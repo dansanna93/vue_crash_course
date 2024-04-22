f5e7536c5e2d90c4b09c962d7a03348bfde2c018
dansanna93 <125999462+dansanna93@users.noreply.github.com>
2024-04-22 18:05:09 +0200

    Added routes and views, v1.0.0 over

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @btn-click="$emit('toggle-add-task')"
+        <Button v-show="homePage"
+         @btn-click="$emit('toggle-add-task')"
          :text="showAddTask ? 'Close' : 'Add Task'" 
          :color="showAddTask ? 'red' : 'green'"/>
     </header>
@@ -17,6 +18,15 @@
         },
         components: {
             Button
+        },
+        computed: {
+            homePage() {
+                if(this.$route.path === '/') {
+                    return true
+                } else {
+                    return false
+                }
+            }
         }
     }
 </script>
